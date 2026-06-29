e72d61f606baa3183f567d55e0a9a8d2b252aded
Mihaja28 <himjaaaime@gmail.com>
2026-06-30 00:21:44 +0300

    Remise en mode de synchronisation alter

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ app.use('/api/enterprise', upgradeRoutes);
 const PORT = process.env.PORT || 5000;
 
 
-sequelize.sync({ force: true }) 
+sequelize.sync({ alter: true }) 
   .then(() => {
     console.log('Base de données PostgreSQL synchronisée !');
     app.listen(PORT, () => {
